e20fda9181c031af7de8a9ec3eb95e4ec1d6ebd7
keeee21 <keisuke.u211016@gmail.com>
2024-12-07 13:20:15 +0900

    feat: オブジェクトごとの依存関係を関数内で検知する

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const handler = {
   get(target, key, receiver) {
     const res = Reflect.get(target, key, receiver);
+    // アクセスされたobjectがtargetという変数で渡ってくる
+    console.log("get", target, key, res);
+    track(target);
     return res;
   },
   set(target, key, value, receiver) {
     const res = Reflect.set(target, key, value, receiver);
-    trigger();
+    console.log("set", target, key, value);
+    trigger(target);
     return res;
   }
 }
@@ -18,10 +22,19 @@ let activeEffect = null;
 
 const effect = (fn) => {
   activeEffect = fn;
+  activeEffect();
 }
 
-const trigger = (fn) => {
-  activeEffect()
+// objectとcallbackを関連付ける
+const targetMap = new WeakMap();
+const track = (target) => {
+  console.log("track", target, "activeEffect", activeEffect);
+  targetMap.set(target, activeEffect);
+}
+
+const trigger = (target) => {
+  const effect = targetMap.get(target);
+  effect();
 }
 
 export { effect, trigger, reactive }
